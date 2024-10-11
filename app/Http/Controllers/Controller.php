1e0e142cf9ab8f1d209136b9a7ab0cf00f67feea
Malco <minayamalco@gmail.com>
2024-10-10 19:16:21 -0500

    cutomer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,7 @@ use Illuminate\Routing\Controller as BaseController;
 use OpenApi\Attributes as OA;
 #[OA\Info(
     version: '1.0.0',
-    title: 'My API AIKO APARTMENTS',
+    title: 'My API SAIKO APARTMENTS',
     attachables: [new OA\Attachable()]
 )]
 #[OA\License(name: 'MIT')]
