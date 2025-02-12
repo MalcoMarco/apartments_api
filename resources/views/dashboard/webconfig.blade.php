2e4655a80ad4eed1dc9c644094ce795a85ee234a
Malco <minayamalco@gmail.com>
2025-02-12 00:54:30 -0500

    bug

--- resources/views/dashboard/webconfig.blade.php
+++ resources/views/dashboard/webconfig.blade.php
@@ -12,7 +12,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h3> Imagenes/Logos</h3>
-                    @json($webConfigs)
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
